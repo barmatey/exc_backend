438cc6fa6b023869a21238c0d1241b3fe0e673c5
barma <barmatey@gmail.com>
2023-11-28 21:17:58 +0300

    deal

--- src/deal/domain.py
+++ src/deal/domain.py
@@ -1,7 +1,7 @@
 from typing import Literal, TypedDict
 from uuid import UUID, uuid4
 
-from pydantic import BaseModel, ConfigDict, PrivateAttr
+from pydantic import BaseModel, ConfigDict, PrivateAttr, Field
 
 from src.base.model import Entity
 from src.core import Ticker
@@ -11,6 +11,7 @@ DealDirection = Literal['BUY', 'SELL']
 
 
 class InnerTransaction(BaseModel):
+    uuid: UUID
     direction: DealDirection
     price: float
     quantity: int
@@ -21,35 +22,15 @@ class Deal(Entity):
     account: UUID
     ticker: Ticker
     status: DealStatus
-    _transactions = PrivateAttr(default_factory=list)
-
-    def __init__(self, transactions: list[InnerTransaction] = None, **data):
-        super().__init__(**data)
-        self._transactions = transactions if transactions is not None else []
-
-    @property
-    def transactions(self) -> list[InnerTransaction]:
-        return self._transactions
-
-    @property
-    def average_price(self) -> float:
-        price = 0
-        quantity = 0
-        for trs in self._transactions:
-            if trs.direction == 'BUY':
-                price += trs.price
-                quantity += trs.quantity
-            else:
-                price -= trs.price
-                quantity -= trs.quantity
-        return price / quantity
-
-    @property
-    def quantity(self) -> int:
-        result = 0
-        for trs in self._transactions:
-            result = result + trs.quantity if trs.direction == 'BUY' else result - trs.quantity
-        return result
+    weighted_price: float
+    total_quantity: int
+    transactions: list[UUID] = Field(default_factory=list)
 
     def append_transaction(self, trs: InnerTransaction):
-        self._transactions.append(trs)
+        self._transactions.append(trs.uuid)
+        self.weighted_price += trs.price * trs.quantity
+        self.total_quantity += trs.quantity
+
+    @property
+    def avg_price(self) -> float:
+        return self.weighted_price / self.total_quantity
--- src/deal/infrastructure/postgres.py
+++ src/deal/infrastructure/postgres.py
@@ -1,14 +1,20 @@
 from uuid import UUID, uuid4
 
-from sqlalchemy import String, TIMESTAMP, Integer, Float, JSON, ForeignKey, select
+from sqlalchemy import String, TIMESTAMP, Integer, Float, JSON, ForeignKey, select, Table, Column
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import UUID
 
-
 from src.base.repo.postgres import Base, PostgresRepo
 from src.base.repo.repository import OrderBy
 from src.deal import domain
+
+association_table = Table(
+    "association_table",
+    Base.metadata,
+    Column("left_id", ForeignKey("left_table.id")),
+    Column("right_id", ForeignKey("right_table.id")),
+)
 
 
 class DealModel(Base):
@@ -16,7 +22,8 @@ class DealModel(Base):
     account: Mapped[UUID] = mapped_column(UUID, nullable=False)
     ticker: Mapped[str] = mapped_column(String(32), nullable=False)
     status: Mapped[str] = mapped_column(String(16), nullable=False)
-    transactions = relationship('InnerTransactionModel', back_populates='deal', lazy='joined')
+    weighted_price: Mapped[float] = mapped_column(Float, nullable=False)
+    total_quantity: Mapped[int] = mapped_column(Integer, nullable=False)
 
     @staticmethod
     def key_converter(key: str):
@@ -30,7 +37,9 @@ class DealModel(Base):
             account=self.account,
             ticker=self.ticker,
             status=self.status,
-            transactions=[x.to_entity() for x in self.transactions]
+            weighted_price=self.weighted_price,
+            total_quantity=self.total_quantity,
+            transactions=self.transactions,
         )
 
     @classmethod
@@ -40,49 +49,7 @@ class DealModel(Base):
             account=entity.account,
             ticker=entity.ticker,
             status=entity.status,
-            transactions=[InnerTransactionModel.from_entity(x) for x in entity.transactions]
+            weighted_price=entity.weighted_price,
+            total_quantity=entity.total_quantity,
+            transactions=entity.transactions,
         )
-
-
-class InnerTransactionModel(Base):
-    __tablename__ = 'deal_transaction_table'
-    price: Mapped[Float] = mapped_column(Float, nullable=False)
-    quantity: Mapped[Integer] = mapped_column(Integer, nullable=False)
-    direction: Mapped[String] = mapped_column(String(8), nullable=False)
-    deal_id: Mapped[UUID] = mapped_column(ForeignKey(DealModel.id))
-    deal = relationship('DealModel', back_populates='transactions')
-
-    @classmethod
-    def from_entity(cls, entity: domain.InnerTransaction):
-        return cls(
-            id=uuid4(),
-            price=entity.price,
-            quantity=entity.quantity,
-            direction=entity.direction,
-        )
-
-    def to_entity(self, **kwargs) -> domain.InnerTransaction:
-        return domain.InnerTransaction(
-            quantity=self.quantity,
-            price=self.price,
-            direction=self.direction,
-        )
-
-
-class DealRepo(PostgresRepo):
-    def __init__(self, session: AsyncSession, model=DealModel):
-        super().__init__(session, model)
-
-    async def get_one_by_id(self, uuid: UUID) -> domain.Deal:
-        raise NotImplemented
-
-    async def get_many_by_id(self, ids: list[UUID], order_by: OrderBy = None) -> list[domain.Deal]:
-        raise NotImplemented
-
-    async def get_many(self, filter_by: dict = None, order_by: OrderBy = None,
-                       slice_from=None, slice_to=None) -> list[domain.Deal]:
-        stmt = select(DealModel)
-        stmt = self._expand_statement(stmt, filter_by, order_by, slice_from, slice_to)
-        result = (await self._session.scalars(stmt)).unique()
-        entities = [model.to_entity() for model in result]
-        return entities
